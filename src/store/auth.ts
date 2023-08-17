796bbcc66fff3a17e7398a03eae4f78eea8f99d6
dasturchioka <dasturchioka@gmail.com>
2023-08-18 01:26:26 +0500

    setting up the auth store

---
+++ src/store/auth.ts
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { authInstance } from "@/http/instances";
+import { toastController } from "@ionic/vue";
+import { Preferences } from "@capacitor/preferences";
+
+export const useAuth = defineStore("auth-store", () => {
+  
+});
