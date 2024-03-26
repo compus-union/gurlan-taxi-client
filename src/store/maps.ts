faaedff49e2bd06ba6c6396789a2d967db0f2240
dasturchioka <dasturchioka@gmail.com>
2024-03-26 06:59:54 +0500

    removed Leaflet's markers

--- src/store/maps.ts
+++ src/store/maps.ts
@@ -87,14 +87,6 @@ export const useMaps = defineStore("maps-store", () => {
             mapLoaded.value = true;
           });
 
-        let originMarker = markers.value.find(
-          (m) => m._custom_id === "origin-marker"
-        ) as CustomMarker;
-
-        let destinationMarker = markers.value.find(
-          (m) => m._custom_id === "destination-marker"
-        ) as CustomMarker;
-
         let realLocationPoint = markers.value.find(
           (m) => m._custom_id === "real-location-point"
         ) as CustomMarker;
@@ -113,16 +105,6 @@ export const useMaps = defineStore("maps-store", () => {
           markers.value.push(realLocationPoint);
         }
 
-        if (!originMarker && route.path === "/ride/setOrigin") {
-          console.log("origin marker is being added in loadMap()");
-          await addOriginMarker();
-        }
-
-        if (!destinationMarker && route.path === "/ride/setDestination") {
-          console.log("destination marker is being added in loadMap*()");
-          await addDestinationMarker();
-        }
-
         return;
       }
     } catch (error) {
@@ -135,118 +117,44 @@ export const useMaps = defineStore("maps-store", () => {
       // get origin marker
 
       sharedMap.value?.addEventListener("drag", async (e) => {
-        let originMarker = markers.value.find(
-          (m) => m._custom_id === "origin-marker"
-        ) as CustomMarker;
-
-        let destinationMarker = markers.value.find(
-          (m) => m._custom_id === "destination-marker"
-        ) as CustomMarker;
         if (route.path === "/ride/letsgo") return;
 
         mapMoving.value = true;
-        const lat = sharedMap.value?.getCenter().lat as number;
-        const lng = sharedMap.value?.getCenter().lng as number;
 
-        if (route.path === "/ride/setOrigin" && originMarker) {
-          originMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
-          return;
-        }
-
-        if (route.path === "/ride/setDestination" && destinationMarker) {
-          destinationMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
-          return;
-        }
+        return;
       });
       sharedMap.value?.addEventListener("move", async (e) => {
-        let originMarker = markers.value.find(
-          (m) => m._custom_id === "origin-marker"
-        ) as CustomMarker;
-
-        let destinationMarker = markers.value.find(
-          (m) => m._custom_id === "destination-marker"
-        ) as CustomMarker;
         if (route.path === "/ride/letsgo") return;
 
         mapMoving.value = true;
-        const lat = sharedMap.value?.getCenter().lat as number;
-        const lng = sharedMap.value?.getCenter().lng as number;
 
-        if (route.path === "/ride/setOrigin" && originMarker) {
-          originMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
-          return;
-        }
-
-        if (route.path === "/ride/setDestination" && destinationMarker) {
-          destinationMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
-          return;
-        }
+        return;
       });
       sharedMap.value?.addEventListener("zoom", async (e) => {
-        let originMarker = markers.value.find(
-          (m) => m._custom_id === "origin-marker"
-        ) as CustomMarker;
-
-        let destinationMarker = markers.value.find(
-          (m) => m._custom_id === "destination-marker"
-        ) as CustomMarker;
         if (route.path === "/ride/letsgo") return;
 
         mapMoving.value = true;
-        const lat = sharedMap.value?.getCenter().lat as number;
-        const lng = sharedMap.value?.getCenter().lng as number;
 
-        if (route.path === "/ride/setOrigin" && originMarker) {
-          originMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
-          return;
-        }
-
-        if (route.path === "/ride/setDestination" && destinationMarker) {
-          destinationMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
-          return;
-        }
+        return;
       });
 
       sharedMap.value?.addEventListener("zoomend", async (e) => {
-        let originMarker = markers.value.find(
-          (m) => m._custom_id === "origin-marker"
-        ) as CustomMarker;
-
-        let destinationMarker = markers.value.find(
-          (m) => m._custom_id === "destination-marker"
-        ) as CustomMarker;
+        mapMoving.value = false;
         if (route.path === "/ride/letsgo") return;
 
         mapMoving.value = false;
         const lat = sharedMap.value?.getCenter().lat as number;
         const lng = sharedMap.value?.getCenter().lng as number;
 
-        if (route.path === "/ride/setOrigin" && originMarker) {
+        if (route.path === "/ride/setOrigin") {
           await originStore.changeCoords({ lat, lng });
 
-          originMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>)
           return;
         }
 
-        if (route.path === "/ride/setDestination" && destinationMarker) {
+        if (route.path === "/ride/setDestination") {
           await destinationStore.changeCoords({ lat, lng }, "void");
-          destinationMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
+
           return;
         }
       });
@@ -282,188 +190,25 @@ export const useMaps = defineStore("maps-store", () => {
       //   }
       // });
       sharedMap.value?.addEventListener("moveend", async (e) => {
-        let originMarker = markers.value.find(
-          (m) => m._custom_id === "origin-marker"
-        ) as CustomMarker;
-
-        let destinationMarker = markers.value.find(
-          (m) => m._custom_id === "destination-marker"
-        ) as CustomMarker;
+        mapMoving.value = false;
         if (route.path === "/ride/letsgo") return;
 
         mapMoving.value = false;
         const lat = sharedMap.value?.getCenter().lat as number;
         const lng = sharedMap.value?.getCenter().lng as number;
 
-        if (route.path === "/ride/setOrigin" && originMarker) {
+        if (route.path === "/ride/setOrigin") {
           await originStore.changeCoords({ lat, lng });
 
-          originMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
           return;
         }
 
-        if (route.path === "/ride/setDestination" && destinationMarker) {
+        if (route.path === "/ride/setDestination") {
           await destinationStore.changeCoords({ lat, lng }, "void");
-          destinationMarker
-            .setLatLng([lat, lng])
-            .addTo(sharedMap.value as Map | LayerGroup<any>);
+
           return;
         }
       });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // remove if origin-marker-fixed marker exists, then add origin-marker
-  async function addOriginMarker() {
-    try {
-      // get fixed marker of origin marker
-      const originMarkerFixed = await findMarker("origin-marker-fixed");
-      // if it exists, remove it
-      if (originMarkerFixed) {
-        await removeMarker(originMarkerFixed);
-      }
-
-      // check if origin marker already exists
-      let originMarker = await findMarker("origin-marker");
-
-      // if doesn't exists, add it
-      if (!originMarker) {
-        const originMarkerIcon = L.icon({
-          iconUrl: OriginMarkerIcon,
-          iconSize: [44, 60],
-          iconAnchor: [22, 60],
-        });
-        originMarker = L.marker(
-          [originCoords.value.lat, originCoords.value.lng],
-          {
-            icon: originMarkerIcon,
-            title: "Siz shu yerda"
-          }
-        );
-        originMarker._custom_id = "origin-marker";
-        originMarker.addTo(sharedMap.value as Map | LayerGroup<any>);
-        markers.value.push(originMarker as CustomMarker);
-        return;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // remove if destination-marker-fixed marker exists, then add destination-marker
-  async function addDestinationMarker() {
-    try {
-      // get fixed marker of destination marker
-      const destinationMarkerFixed = await findMarker(
-        "destination-marker-fixed"
-      );
-      // if it exists, remove it
-      if (destinationMarkerFixed) {
-        await removeMarker(destinationMarkerFixed);
-      }
-
-      // check if destination marker already exists
-      let destinationMarker = await findMarker("destination-marker");
-
-      // if doesn't exists, add it
-      if (!destinationMarker) {
-        const destinationIcon = L.icon({
-          iconUrl: DestinationMarkerIcon,
-          iconSize: [44, 60],
-          iconAnchor: [22, 60],
-        });
-
-        destinationMarker = L.marker(
-          [
-            destinationCoords.value.lat
-              ? destinationCoords.value.lat
-              : originCoords.value.lat,
-            destinationCoords.value.lng
-              ? destinationCoords.value.lng
-              : originCoords.value.lng,
-          ],
-          {
-            icon: destinationIcon,
-          }
-        );
-
-        destinationMarker._custom_id = "destination-marker";
-        destinationMarker.addTo(sharedMap.value as Map | LayerGroup<any>);
-        markers.value.push(destinationMarker as CustomMarker);
-
-        return;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // remove if origin-marker exists, then add origin-marker-fixed
-  async function addFixedOriginMarker() {
-    try {
-      const originMarker = await findMarker("origin-marker");
-
-      if (originMarker) {
-        await removeMarker(originMarker);
-      }
-
-      const originMarkerIcon = L.icon({
-        iconUrl: OriginMarkerIcon,
-        iconSize: [44, 60],
-        iconAnchor: [22, 60],
-      });
-
-      const fixedOriginMarker = L.marker(
-        [originCoords.value.lat, originCoords.value.lng],
-        {
-          icon: originMarkerIcon,
-        }
-      ) as CustomMarker;
-
-      fixedOriginMarker._custom_id = "origin-marker-fixed";
-      fixedOriginMarker.addTo(sharedMap.value as Map);
-      markers.value.push(fixedOriginMarker);
-      return;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  // remove if destination-marker exists, then add destination-marker-fixed
-  async function addFixedDestinationMarker() {
-    try {
-      const destinationMarker = await findMarker("destination-marker");
-
-      if (destinationMarker) {
-        await removeMarker(destinationMarker);
-      }
-
-      const destinationIcon = L.icon({
-        iconUrl: DestinationMarkerIcon,
-        iconSize: [44, 60],
-        iconAnchor: [22, 60],
-      });
-
-      const fixedDestinationMarker = L.marker(
-        [
-          destinationCoords.value.lat
-            ? destinationCoords.value.lat
-            : originCoords.value.lat,
-          destinationCoords.value.lng
-            ? destinationCoords.value.lng
-            : originCoords.value.lng,
-        ],
-        { icon: destinationIcon }
-      ) as CustomMarker;
-
-      fixedDestinationMarker._custom_id = "destination-marker-fixed";
-      fixedDestinationMarker.addTo(sharedMap.value as Map);
-      markers.value.push(fixedDestinationMarker);
-      return;
     } catch (error) {
       console.log(error);
     }
@@ -477,11 +222,7 @@ export const useMaps = defineStore("maps-store", () => {
     defaultZoom,
     mapMoving,
     initialiseEvents,
-    addFixedOriginMarker,
-    addDestinationMarker,
-    addFixedDestinationMarker,
-    addOriginMarker,
     mapLoaded,
-    findMarker
+    findMarker,
   };
 });
